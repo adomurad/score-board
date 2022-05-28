989411ae50285eeb916d483e63566f61d48f3f79
Artur Domurad <domurad.artur@gmail.com>
2022-05-28 12:36:56 +0200

    team names - implementation

--- src/score-board/score-board.service.ts
+++ src/score-board/score-board.service.ts
@@ -6,7 +6,7 @@ export class ScoreBoard {
   constructor(private store: IScoreProvider) { }
 
   public startGame(homeTeamName: string, awayTeamName: string): void {
-    const newGame = {};
+    const newGame = this.createNewGameEntry(homeTeamName, awayTeamName);
     this.store.setCurrentGame(newGame);
   }
 
@@ -15,4 +15,14 @@ export class ScoreBoard {
     return currentGame;
   }
 
+  private createNewGameEntry(homeTeamName: string, awayTeamName: string): GameEntry {
+    return {
+      homeTeam: {
+        teamName: homeTeamName,
+      },
+      awayTeam: {
+        teamName: awayTeamName,
+      }
+    };
+  }
 }
