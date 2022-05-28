ee5c900ef5fc2468a02ae0f0736b5dd235ad9970
Artur Domurad <domurad.artur@gmail.com>
2022-05-28 13:30:44 +0200

    game comparer - date - implementation

--- src/score-board/compare-games.ts
+++ src/score-board/compare-games.ts
@@ -4,6 +4,11 @@ export function compareGames(a: GameEntry, b: GameEntry): number {
   const aScoreSum = sumScores(a);
   const bScoreSum = sumScores(b);
 
+  if (aScoreSum === bScoreSum) {
+    const deb1 = b.startDateTimestamp - a.startDateTimestamp;
+    return deb1;
+  }
+
   return bScoreSum - aScoreSum;
 }
 
