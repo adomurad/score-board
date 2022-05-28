3b8fab511201a3ee2d6ed63bbed738a4d4675a7e
Artur Domurad <domurad.artur@gmail.com>
2022-05-28 13:20:16 +0200

    game comparer - test

---
+++ src/score-board/compare-games.test.ts
@@ -0,0 +1,28 @@
+import { GameEntry } from '../score-provider/game-entry.interface';
+import { compareGames } from './compare-games';
+
+function getMockedGame(score1: number, score2: number): GameEntry {
+  return {
+    homeTeam: {
+      teamName: '1',
+      score: score1,
+    },
+    awayTeam: {
+      teamName: '2',
+      score: score2,
+    },
+  };
+}
+
+describe('compare-games.test.ts tests', () => {
+
+  test('should return the difference in score sums', () => {
+    const game1 = getMockedGame(6, 7);
+    const game2 = getMockedGame(10, 0);
+
+    const result = compareGames(game1, game2);
+
+    expect(result).toBe(-3);
+  });
+
+});
---
+++ src/score-board/compare-games.ts
@@ -0,0 +1,5 @@
+import { GameEntry } from '../score-provider/game-entry.interface';
+
+export function compareGames(a: GameEntry, b: GameEntry): number {
+  return 0;
+}
