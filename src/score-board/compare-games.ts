94739c76347215601acf6d762635325a4d7b2388
Artur Domurad <domurad.artur@gmail.com>
2022-05-28 13:20:56 +0200

    game comparer - implementation

--- src/score-board/compare-games.ts
+++ src/score-board/compare-games.ts
@@ -1,5 +1,12 @@
 import { GameEntry } from '../score-provider/game-entry.interface';
 
 export function compareGames(a: GameEntry, b: GameEntry): number {
-  return 0;
+  const aScoreSum = sumScores(a);
+  const bScoreSum = sumScores(b);
+
+  return bScoreSum - aScoreSum;
+}
+
+function sumScores(game: GameEntry): number {
+  return game.homeTeam.score + game.awayTeam.score;
 }
